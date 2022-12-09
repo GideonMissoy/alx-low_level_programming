ad04b52065d9b509e53e35f61cc506124e2e47d9
Gideon Kipsang <missoykipsang@gmail.com>
2022-12-08 16:31:00 -0800

    Write a C program that prints

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ **/
+
+int main(void)
+
+{
+puts("\"Programming is lik` building a`multilingual puzzle");
+	return (0);
+}
+
