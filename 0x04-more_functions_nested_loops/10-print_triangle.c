5ea117d4f48638e4f3ade04ae74bc8f510860441
Gideon Kipsang <missoykipsang@gmail.com>
2022-12-16 18:22:26 -0800

    Fixing parameters.

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,21 +8,19 @@
 
 void print_triangle(int size)
 {
-	int l;
-	int h;
-	int s;
+	int p, q, spaces;
 
 	if (size <= 0)
 		_putchar('\n');
 	else
 	{
-		for (l = 1; l <= size; l++)
+		for (p = 1; p <= size; p++)
 		{
-			for (s = size - l; s >= l; s--)
+			for (spaces = size - p; spaces >= 1; spaces--)
 			{
 				_putchar(' ');
 			}
-			for (h = 1; h <= l; h++)
+			for (q = 1; q <= p; q++)
 			{
 				_putchar('#');
 			}
