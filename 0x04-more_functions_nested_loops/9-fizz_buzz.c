fa51e6673b75aade8eedd81cdba0e277c1c2df5e
Gideon Kipsang <missoykipsang@gmail.com>
2022-12-16 18:34:10 -0800

    Hot fix for spaces in FizzBuzz.

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,6 +3,7 @@
 /**
  * main - prints the numbers from 1 to 100,
  * except multiples of 3 and 5
+ * Return: Always 0
  */
 
 int main(void)
@@ -11,16 +12,16 @@ int main(void)
 
 	for (q = '1'; q <= 100; q++)
 	{
-		if (q == 100)
-			printf("Buzz");
-		else if (q % 3 == 0 && q % 5 == 0)
+		if (q % 15 == 0)
 			printf("FizzBuzz");
 		else if (q % 3 == 0)
 			printf("Fizz");
-		else if (q != 100 && q % 5 == 0)
+		else if (q % 5 == 0)
 			printf("Buzz");
 		else
-			printf("%d ", q);
+			printf("%d", q);
+		if (q < 100)
+			printf(" ");
 	}
 	printf("\n");
 }
