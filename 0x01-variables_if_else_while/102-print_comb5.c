67502135e7178e6a8759432aef981df78a478101
Gideon Kipsang <missoykipsang@gmail.com>
2022-12-16 19:07:18 -0800

    Hot fix for 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,18 +12,18 @@ int main(void)
 	int p;
 	int q;
 
-	for (p = '0'; p < 100; p++)
+	for (p = 0; p < 100; p++)
 	{
-		for (q = '0'; q < 100; q++)
+		for (q = 0; q < 100; q++)
 		{
 			if (p < q)
 			{
-				putchar((p / 10) + '0');
-				putchar((p % 10) + '0');
+				putchar((p / 10) + 48);
+				putchar((p % 10) + 48);
 				putchar(' ');
-				putchar((q / 10) + '0');
-				putchar((q % 10) + '0');
-				if (p != 98 && q != 99)
+				putchar((q / 10) + 48);
+				putchar((q % 10) + 48);
+				if (p != 98 ||	q != 99)
 				{
 					putchar(',');
 					putchar(' ');
