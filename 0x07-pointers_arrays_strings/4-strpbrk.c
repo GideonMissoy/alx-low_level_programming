fefdf1c166848bc75eefadf1f1e16138c1fcdfe5
Gideon Kipsang <missoykipsang@gmail.com>
2023-01-04 14:17:16 -0800

    This function searches a string for any of a set of bytes and locates the first occurrence in the string s of any of the bytes in the string accept then returns a pointer to the byte in s that matches one of the bytes in accept, or NULL if no such byte is found.

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: string to be checked.
+ * @accept: string to be used
+ * Return: a pointer to the byte
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int p;
+
+	while (*s)
+	{
+		for (p = 0; accept[p]; p++)
+		{
+			if (*s == accept[p])
+				return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
