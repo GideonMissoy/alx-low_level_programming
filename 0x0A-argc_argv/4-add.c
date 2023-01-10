8194c80476341154330c8746b570456eb16796f1
Gideon Kipsang <missoykipsang@gmail.com>
2023-01-09 16:31:21 -0800

    To infinity and beyond. This program adds positive numbers.

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: n args
+ * @argv: arr args
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	unsigned int p, sum, num;
+
+	sum = 0;
+
+	if (argc < 3)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	while (argc-- && argc > 0)
+	{
+		for (p = 0; argv[argc][p] != '\0'; p++)
+		{
+			if (!(isdigit(argv[argc][p])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		num = atoi(argv[argc]);
+		sum += num;
+	}
+	printf("%d\n", sum);
+	return (sum);
+}
