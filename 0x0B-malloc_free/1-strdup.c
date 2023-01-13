e6ca4940e09e89d3738e675f9c35f059a75c61f5
Gideon Kipsang <missoykipsang@gmail.com>
2023-01-12 18:22:40 -0800

    The woman who has no imagination has no wings. This function returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,42 @@
 #include "main.h"
 
 /**
+ * _strlen - counts string
+ * @s: string
+ * Return: 1
+ */
+
+int _strlen(char *s)
+{
+	unsigned int p;
+
+	p = 0;
+	while (s[p] != '\0')
+		p++;
+	return (p);
+}
+
+/**
+ * _strcpy - copies string
+ * @dest: destination
+ * @src: source
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int p = 0;
+
+	while (src[p] != '\0')
+	{
+		dest[p] = src[p];
+		p++;
+	}
+	dest[p] = '\0';
+	return (dest);
+}
+
+/**
  * _strdup - function returns a pointer to a newly allocated space in memory
  * @str: string to copy
  * Return: pointer to the dupliated string, NULL otherwise
@@ -8,23 +44,19 @@
 
 char *_strdup(char *str)
 {
-	unsigned int p, q;
+	unsigned int p;
 	char *r;
 
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	else
-		for (p = 0; str[p]; p++)
-			p++;
-	r = malloc(p * sizeof(char));
+	p = _strlen(str) + 1;
+	r = (char *)malloc(p * sizeof(char));
 	if (r == NULL)
 	{
 		return (NULL);
 	}
-	else
-		for (q = 0; q < 1; q++)
-			r[q] = str[q];
+	_strcpy(r, str);
 	return (r);
 }
