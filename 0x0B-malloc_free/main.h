ca23e4a8129f953ebd2970f35e5e04781fe2ad5c
Gideon Kipsang <missoykipsang@gmail.com>
2023-01-12 17:57:18 -0800

    Updating main.h file.

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -11,5 +11,6 @@ char *str_concat(char *s1, char *s2);
 void free_grid(int **grid, int height);
 int **alloc_grid(int width, int height);
 char *create_array(unsigned int size, char c);
+int _putchar(char c);
 
 #endif
