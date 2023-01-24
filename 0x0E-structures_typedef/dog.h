a35c1586d45064cf53eca38c866d168e913a9e91
Gideon Kipsang <missoykipsang@gmail.com>
2023-01-23 16:14:09 -0800

    Updating my dog.h file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -26,7 +26,8 @@ int _putchar(char);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+void free_dog(dog_t *d)i;
+char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
 #endif
