11d83bf0286d092d827029249c7061e0091dbe9b
Gideon Kipsang <missoykipsang@gmail.com>
2023-01-25 17:18:43 -0800

    A goal is not always meant to be reached, it often serves simply as something to aim at. This program performs simple operations.

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -22,11 +22,9 @@ int (*get_op_func(char *s))(int, int)
 
 	while (i < 5)
 	{
-		if (strcmp(s, ops[i].op == 0))
-				return (ops[i].f);
-
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
 		i++;
 	}
-
 	return (0);
 }
