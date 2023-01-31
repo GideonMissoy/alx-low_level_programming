239ebe39fbeba102578bfdfb6a12fc176e84fa1d
GideonMissoy <missoykipsang@gmail.com>
2023-01-30 18:58:04 -0800

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,16 +1,16 @@
 #include "lists.h"
 
 /**
- * theHair_tortoise - apply attribute so its printed before main
+ * myStartupFun - apply attribute so its printed before main
  */
 
-void theHair_tortoise(void) _attribute_ ((constructor));
+void myStartupFun(void) _attribute_ ((constructor));
 
 /**
- * theHair_tortoise - implementation
+ * myStartupFun - implementation
  */
 
-void theHair_tortoise(void)
+void myStartupFun(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
