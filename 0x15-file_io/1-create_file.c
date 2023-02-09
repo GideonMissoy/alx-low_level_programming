e5cf790a3ecefd08da6695054ebbb5bc90516790
GideonMissoy <missoykipsang@gmail.com>
2023-02-08 19:09:49 -0800

    Under the snow, This function creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,13 +9,15 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int p, q, r;
+	int p, q, r = 0;
 
 	if (filename == NULL)
 		return (-1);
 	if (text_content != NULL)
+	{
 		for (r = 0; text_content[r];)
 			r++;
+	}
 
 	p = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	q = write(p, text_content, r);
