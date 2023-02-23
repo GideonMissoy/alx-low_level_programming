da78de6a97797cfb260e0d7fd5c080f3df8fd1fa
GideonMissoy <missoykipsang@gmail.com>
2023-02-23 15:32:40 -0800

    Adding main file.

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,10 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdlib.h>
+void _print(char *str, int l);
+char *mul(char n, char *num, int num_index, char *dest, int dest_index);
+int check_for_digits(char **av);
+void init(char *str, int l);
 #include <stdio.h>
-#include <limits.h>
-
+#include <stdlib.h>
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
