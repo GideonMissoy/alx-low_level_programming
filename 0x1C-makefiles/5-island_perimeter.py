dc5d3d620233c9933b6810bc768f4f2f4bee7935
GideonMissoy <giddykips2030@gmail.com>
2023-07-30 15:19:58 -0700

    5. Island perimeter. Function returns the perimeter of the island described in grid.

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+"""
+Defines an island perimeter measuring function.
+"""
+
+
+def island_perimeter(grid):
+    """Return the perimeter of an island.
+    The grid represents water 0 and land 1.
+    Returns the perimeter of the island."""
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for p in range (height):
+        for q in range(width):
+            if grid[p][q] == 1:
+                size += 1
+                if (q > 0 and grid[p][q-1] == 1):
+                    edges += 1
+                if (p > 0 and grid[p - 1][q] == 1):
+                    edges += 1
+    return (size * 4 - edges * 2)
